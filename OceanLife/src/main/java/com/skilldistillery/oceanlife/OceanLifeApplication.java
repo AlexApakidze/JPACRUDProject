d44ff0181466f5724ba1a871748741cf3d9e5ecf
Alex A <Alexapakidze@yahoo.com>
2023-07-14 17:29:11 -0600

    set up spring

---
+++ OceanLife/src/main/java/com/skilldistillery/oceanlife/OceanLifeApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.oceanlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceanLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OceanLifeApplication.class, args);
+	}
+
+}
---
+++ OceanLife/src/test/java/com/skilldistillery/oceanlife/OceanLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.oceanlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OceanLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
